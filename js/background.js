9b4b5d2643b1a4e32433efcd0301a9c47decbd2c
Ivan Jovanovic <ij.smartnet@gmail.com>
2016-03-15 12:10:15 +0100

    MPS-52 implemented functionality for badge text

---
+++ js/background.js
@@ -0,0 +1,73 @@
+(function() {
+  'use strict';
+
+  var localStorage = chrome.storage.local;
+  var apiUrl;
+  var intervalId = false;
+
+  function refresh() {
+    localStorage.get('apiUrl', function(data) {
+      if (data && data.apiUrl) {
+        updateBadge(data.apiUrl);
+        if (intervalId) {
+          clearInterval(intervalId);
+        }
+        intervalId = setInterval(updateBadge, 60000, data.apiUrl);
+      }
+    });
+  }
+
+  function updateBadge(apiUrl) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+    xhr.withCredentials = true;
+
+    xhr.open("GET", apiUrl, true, "test", "123test");
+
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        var status = xhr.status;
+        var statusText = xhr.statusText;
+
+        if (status === 200 && statusText === "OK") {
+          var response = xhr.response;
+          if (response.ErrorCode === 0) {
+            var count = response.Data.visibleCardInstancesCount;
+            chrome.browserAction.setBadgeText({
+              text: count + ""
+            });
+            chrome.browserAction.setIcon({
+              path: '../img/icon-64px.png'
+            });
+          } else if (response.ErrorCode === 401) {
+            // set error icon on browserAction
+            var errorText = response.Data.ErrorText;
+            chrome.browserAction.setIcon({
+              path: '../img/error-icon-64px.png'
+            });
+          }
+        } else {
+          // set error icon on browserAction
+          chrome.browserAction.setIcon({
+            path: '../img/error-icon-64px.png'
+          });
+        }
+      }
+    };
+
+    xhr.send();
+  }
+
+  function Application() {
+    return {
+      refresh: function() {
+        refresh();
+      }
+    };
+  }
+
+  window.Application = Application();
+
+  refresh();
+
+}());
--- js/options.js
+++ js/options.js
@@ -5,6 +5,13 @@
   var urlEcho = document.getElementById('urlEcho');
   var localStorage = chrome.storage.local;
 
+  var apiUrlSaveButton = document.getElementById('apiUrlSaveBtn');
+  var apiUrlInput = document.getElementById('apiUrlInput');
+  var apiUrlEcho = document.getElementById('apiUrlEcho');
+
+  var backgroundPage = chrome.extension.getBackgroundPage();
+  var application = backgroundPage.Application;
+
   if (saveButton) {
     saveButton.addEventListener('click', function(event) {
       var frameUrl = urlInput.value;
@@ -29,4 +36,23 @@
     }
   });
 
+  if (apiUrlSaveButton) {
+    apiUrlSaveButton.addEventListener('click', function(event) {
+      var apiUrl = apiUrlInput.value;
+      localStorage.set({
+        apiUrl: apiUrl
+      });
+      apiUrlEcho.innerHTML = apiUrl;
+      if (application && application.refresh) {
+        application.refresh();
+      }
+    });
+  }
+
+  localStorage.get('apiUrl', function (data) {
+    if (data && data.apiUrl) {
+      apiUrlEcho.innerHTML = data.apiUrl;
+      apiUrlInput.value = data.apiUrl;
+    }
+  });
 }());
